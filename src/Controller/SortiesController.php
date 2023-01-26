ec4cb42cd8a26f0c3ef0cdb5fb5630ebaae6be21
ClaudeEni <107407888+ClaudeEni@users.noreply.github.com>
2023-01-26 12:02:31 +0100

    begin modify sorties method

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -7,6 +7,7 @@ use App\Entity\Sortie;
 use App\Form\CreerSortieType;
 use App\Repository\EtatRepository;
 use App\Repository\ParticipantRepository;
+use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,9 +27,52 @@ class SortiesController extends AbstractController
     /**
      * @Route("/sorties/creerSortie", name="sorties_creerSortie")
      */
-    public function creerSortie(Request $request, EntityManagerInterface $entityManager, EtatRepository $etatRepository, ParticipantRepository $participantRepository): Response
+    public function creerSortie(Request $request, EntityManagerInterface $entityManager, EtatRepository $etatRepository): Response
     {
         $sortie = new Sortie();
+        $user =$this->getUser();
+        $sortie->setParticipantOrganisateur($user);
+        $sortie->setCampus($user->getCampus());
+        $etatCreee = $etatRepository->findOneBy(["libelle"=>"Créée"]);
+        $etatOuverte = $etatRepository->findOneBy(["libelle"=>"Ouverte"]);
+        $creerSortieForm = $this->createForm(CreerSortieType::class, $sortie);
+
+        $creerSortieForm->handleRequest($request);
+
+        if ($creerSortieForm->get('enregistrer')->isClicked()) {
+            $sortie->setEtat($etatCreee);
+            $message = 'Votre sortie a été créée avec succès';
+        }elseif ($creerSortieForm->get('publier')->isClicked()){
+            $sortie->setEtat($etatOuverte);
+            $message = 'Votre sortie a été publiée avec succès';
+        }
+
+        if ($creerSortieForm->isSubmitted() && $creerSortieForm->isValid()){
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+            $this->addflash('success', $message);
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('sorties/creerSortie.html.twig', [
+            "user" => $user,
+            "creerSortieForm" => $creerSortieForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/sorties/creerSortie", name="sorties_creerSortie")
+     */
+    public function modifierSortie(Request $request, EntityManagerInterface $entityManager, SortieRepository $sortieRepository, int $id): Response
+    {
+        $sortie = $sortieRepository->find($id);
+        $etat = $sortie->getEtat()->getLibelle();
+        $organisateur = $sortie->getParticipantOrganisateur() === $this->getUser();
+
+        if ($organisateur && $sortie != null && $etat == "Créée"){
+
+        }
+
         $user =$this->getUser();
         $sortie->setParticipantOrganisateur($user);
         $sortie->setCampus($user->getCampus());
