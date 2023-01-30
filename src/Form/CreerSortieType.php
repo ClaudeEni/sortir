89fbb0445ee3b8b230c80b5ae35eee94c49da433
ClaudeEni <107407888+ClaudeEni@users.noreply.github.com>
2023-01-30 20:05:43 +0100

    affichage dynamique des dropdownlists presque fonctionnel

--- src/Form/CreerSortieType.php
+++ src/Form/CreerSortieType.php
@@ -16,6 +16,9 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CreerSortieType extends AbstractType
@@ -30,8 +33,8 @@ class CreerSortieType extends AbstractType
             ->add('duree', IntegerType::class, ['label'=>'Durée'])
             ->add('infosSortie', TextareaType::class, ['label'=>'Description et infos'])
             //->add('campus', EntityType::class, ['label'=>'Campus', 'class'=>Campus::class])
-            ->add('ville', EntityType::class, ['label'=>'Ville', 'class'=>Ville::class, 'choice_label'=>'nom', 'mapped'=>false])
-            ->add('lieu', EntityType::class, ['label'=>'Lieu', 'class'=>Lieu::class, 'choice_label'=>'nom'])
+            ->add('ville', EntityType::class, ['placeholder'=>'Veuillez choisir une ville', 'label'=>'Ville', 'class'=>Ville::class, 'choice_label'=>'nom', 'mapped'=>false])
+            ->add('lieu', ChoiceType::class, ['placeholder'=>'Veuillez choisir un lieu'])
             //->add('rue', EntityType::class, ['label'=>'Rue', 'class'=>'App\Entity\Lieu', 'choice_label'=>'rue']) TODO: les 4 champs suivants doivent être gérés en front et seront affichés après avoir sélectionné le lieu
             //->add('codePostal', TextType::class, ['label'=>'Code postal'])
             //->add('latitude', TextType::class, ['label'=>'Latitude'])
@@ -39,6 +42,25 @@ class CreerSortieType extends AbstractType
             ->add('enregistrer', SubmitType::class, ['label'=>'Enregistrer'])
             ->add('publier', SubmitType::class, ['label'=>'Publier la sortie'])
         ;
+
+        $formModifier = function (FormInterface $form, Ville $ville = null){
+            $lieux = (null === $ville) ? [] : $ville->getLieus();
+            $form->add('lieu', EntityType::class, [
+                'class'=>Lieu::class,
+                'choices'=>$lieux,
+                'choice_label'=>'nom',
+                'placeholder'=>'Veuillez choisir un lieu',
+                'label'=>'Lieu'
+            ]);
+        };
+
+        $builder->get('ville')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) use ($formModifier){
+                $ville = $event->getForm()->getData();
+                $formModifier($event->getForm()->getParent(), $ville);
+            }
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
