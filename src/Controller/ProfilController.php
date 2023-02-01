8e5db5ccf8d9be9cab0268be966d786c7730a739
JMoreau2022 <julien.moreau2022@campus-eni.fr>
2023-02-01 17:54:55 +0100

    Import d'utilisateurs

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Form\CreerParticipantType;
+use App\Form\ImportParticipantType;
 use App\Form\ModificationProfilMDPType;
 use App\Form\ModificationProfilType;
+use App\Repository\CampusRepository;
 use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -34,15 +36,77 @@ class ProfilController extends AbstractController
      */
     public function nouveauProfil(Request $request,
                                   UserPasswordHasherInterface $passwordHasher,
-                                  EntityManagerInterface $entityManager) : Response {
+                                  EntityManagerInterface $entityManager,
+                                  ModificationProfilType $modificationProfilType,
+                                  ParticipantRepository $participantRepository,
+                                  CampusRepository $campusRepository) : Response {
         $participant = new Participant();
-        dump($participant);
 
         // création du formulaire pour saisie des informations
         $nouveauProfil = $this->createForm(CreerParticipantType::class, $participant);
         $nouveauProfil->handleRequest($request);
 
+        $importParticipantForm = $this->createForm(ImportParticipantType::class);
+        $importParticipantForm->handleRequest($request);
+
+
+        if ($importParticipantForm->get('importer')->isClicked()) {
+
+            $file = $importParticipantForm->get('import')->getData();
+
+            // Open the file
+            if (($handle = fopen($file->getPathname(), "r")) !== false) {
+                $user = 0;
+                $userko = 0;
+                while (($data = fgetcsv($handle,1024,';')) !== false) {
+
+                    // recherche du campus
+                    $campus = $campusRepository->findOneBy(["nom"=>$data[0]]);
+                    // un participant est-il déjà créé avec ce mail ou ce pseudo
+                    $participant_mail = $participantRepository->findOneBy(["mail"=>$data[4]]);
+                    $participant_pseudo = $participantRepository->findOneBy(["pseudonyme"=>$data[5]]);
+
+                    // si un participant existe avec le même mail ou pseudo, on ne l'ajoute pas
+                    if ($participant_mail or $participant_pseudo){
+                        $userko++;
+                    }
+                    else{
+                        $participant = new Participant();
+
+                        $participant->setCampus($campus);
+                        $participant->setNom($data[1]);
+                        $participant->setPrenom($data[2]);
+                        $participant->setTelephone($data[3]);
+                        $participant->setMail($data[4]);
+                        $participant->setPseudonyme($data[5]);
+                        $participant->setPassword($passwordHasher->hashPassword($participant,$data[6]));
+                        $participant->setAdministrateur(false);
+                        $participant->setActif(true);
+                        $entityManager->persist($participant);
+                        $entityManager->flush();
+                        $user++;
+                    }
+                }
+                if($user>0){
+                    $message= ($user===1) ? " utilisateur a été créé avec succès" : " utilisateurs ont été créés avec succès";
+                    $this->addflash('success', $user.$message);
+                }
+                if($userko>0){
+                    $messageko= ($userko===1) ? " utilisateur n'a pas pu être créé" : " utilisateurs n'ont  pas pu être créés"." Le mail et/ou le pseudo existe déjà !";
+                    $this->addflash('warning', $userko.$messageko);
+                }
+                fclose($handle);
+            }
+
+            return $this->render('profil/CreerProfil.html.twig', [
+                "participant" => $participant,
+                "participantForm" => $nouveauProfil->createView(),
+                "participantImportForm"=>$importParticipantForm->createView()
+            ]);
+        }
+
         if ($nouveauProfil->isSubmitted() and $nouveauProfil->isValid()) {
+
             $participant->setActif(true);
             $participant->setPassword(
                 $passwordHasher->hashPassword($participant, $nouveauProfil->get('password')->getData())
@@ -55,10 +119,49 @@ class ProfilController extends AbstractController
 
         return $this->render('profil/CreerProfil.html.twig', [
             "participant" => $participant,
-            "participantForm" => $nouveauProfil->createView()
+            "participantForm" => $nouveauProfil->createView(),
+            "participantImportForm"=>$importParticipantForm->createView()
         ]);
     }
 
+    /**
+     * @Route ("profil/import",name="profil_import")
+     */
+    public function uploadUser(Request $request) : Response{
+
+        $participant = new Participant();
+
+        // création du formulaire pour saisie des informations
+        $nouveauProfil = $this->createForm(CreerParticipantType::class, $participant);
+        $nouveauProfil->handleRequest($request);
+
+        echo('Début de l\'import');
+        $file = $nouveauProfil->get('import')->getData();
+        if ($file) {
+            echo("fichier sélectionné");
+            // Open the file
+            if (($handle = fopen($file->getPathname(), "r")) !== false) {
+                echo("fichier : ".$file->getPathname());
+                // Read and process the lines.
+                // Skip the first line if the file includes a header
+//                while (($data = fgetcsv($handle)) !== false) {
+//                    // Do the processing: Map line to entity, validate if needed
+//                    $entity = new Entity();
+//                    // Assign fields
+//                    $entity->setField1($data[0]);
+//                    $em->persist($entity);
+//                }
+//                fclose($handle);
+//                $em->flush();
+            }
+        }
+
+
+        return $this->render('profil/CreerProfil.html.twig', [
+            "participant" => $participant,
+            "participantForm" => $nouveauProfil->createView()
+        ]);
+    }
 
     /**
      * @Route("/profil/{id}", name="profil_afficher")
--- src/Form/CreerParticipantType.php
+++ src/Form/CreerParticipantType.php
@@ -6,9 +6,12 @@ use App\Entity\Campus;
 use App\Entity\Participant;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class CreerParticipantType extends AbstractType
 {
@@ -32,7 +35,8 @@ class CreerParticipantType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Participant::class,
+            'data_class' => Participant::class
+//            'attr'=>['novalidate'=>'novalidate']  // pour empêcher la validation du formulaire HTML et laisser les validators
         ]);
     }
 }
