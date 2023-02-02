b0d3ef3e8ff56adc3bac0b985764a1858272c1fc
JMoreau2022 <julien.moreau2022@campus-eni.fr>
2023-02-02 12:24:57 +0100

    fix disabled sur téléphone

--- src/Form/ModificationProfilType.php
+++ src/Form/ModificationProfilType.php
@@ -6,6 +6,7 @@ use App\Entity\Campus;
 use App\Entity\Participant;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
